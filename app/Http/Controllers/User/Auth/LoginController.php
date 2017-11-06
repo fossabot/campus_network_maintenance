4aba07388c56a59faf825c29dfecffff22c9cc35
StarUDream <840175198@qq.com>
2017-11-06 10:34:23 +0800

    添加用户界面依赖

---
+++ app/Http/Controllers/User/Auth/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\User\Auth;
+
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    public function show()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+}

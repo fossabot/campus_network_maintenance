c1dc18e77d644e9a34185a024391210b8154af62
StarUDream <840175198@qq.com>
2017-11-16 11:20:40 +0800

    修改维修备件

--- app/Http/Controllers/Admin/Part/UpdateController.php
+++ app/Http/Controllers/Admin/Part/UpdateController.php
@@ -3,11 +3,48 @@
 namespace App\Http\Controllers\Admin\Part;
 
 use App\Http\Controllers\Controller;
+use App\Models\Part;
+use Illuminate\Http\Request;
 
 class UpdateController extends Controller
 {
-    public function update()
+    public function update(Request $request)
     {
+        $name = $request->input('name');
 
+        if (mb_strlen($name) > 64) {
+            return response()->json('维修备件的长度不得超过64个字符。', 422);
+        }
+
+        if ($this->checkNameUnique($request)) {
+            return response()->json('维修备件名称 已经存在。', 422);
+        }
+
+        if ($this->attemptCreate($request)) {
+            return response()->json();
+        }
+
+        return response()->json('服务器错误。', 500);
+    }
+
+
+    /**
+     * @param Request $request
+     *
+     * @return bool
+     */
+    protected function checkNameUnique(Request $request)
+    {
+        return (bool)Part::whereName($request->input('name'))->whereKeyNot($request->input('id'))->first();
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return bool
+     */
+    protected function attemptCreate(Request $request)
+    {
+        return Part::findOrFail($request->input('id'))->forceFill($request->only('name'))->save();
     }
 }
--- resources/assets/js/pages/admin/part/Detail.vue
+++ resources/assets/js/pages/admin/part/Detail.vue
@@ -1,5 +1,75 @@
 <template>
-    <div class="repair-create">
-        create
+    <div class="type-detail">
+        <el-row>
+            <el-col :md="12">
+                <el-form :model="data" :rules="rules" ref="data" label-width="120px">
+                    <el-form-item label="备件名称" prop="name">
+                        <el-input v-model="data.name"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" :loading="lock" @click="submitForm('data')">修改</el-button>
+                        <el-button @click="resetForm('data')">重置</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+        </el-row>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                lock: false,
+                data: {
+                    name: ''
+                },
+                rules: {
+                    name: [
+                        {required: true, message: '请输入备件名称', trigger: 'blur'},
+                        {max: 64, message: '维修备件的长度不得超过64个字符', trigger: 'blur'}
+                    ]
+                }
+            }
+        },
+        methods: {
+            getData() {
+                this.$http.get(
+                    '/api/admin/part/detail/' + this.$route.params.id
+                ).then((response) => {
+                    if (response.status === 200) {
+                        this.data = response.data
+                        this.$message.success({
+                            message: '获取成功'
+                        })
+                    }
+                })
+            },
+            submitForm(data) {
+                this.$refs[data].validate((valid) => {
+                    if (valid) {
+                        this.lock = true
+                        this.$http.post(
+                            '/api/admin/part/update', this.data
+                        ).then((response) => {
+                            this.lock = false
+                            if (response.status === 200) {
+                                this.$notify.success({
+                                    message: '修改成功',
+                                    duration: 2000
+                                })
+                                this.$router.replace('/part/list')
+                            }
+                        })
+                    }
+                })
+            },
+            resetForm(data) {
+                this.$refs[data].resetFields()
+            }
+        },
+        mounted() {
+            this.getData()
+        }
+    }
+</script>

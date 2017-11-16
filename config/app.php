4b44f44d47e427bc31f25d09f0c31b8ede1670be
StarUDream <840175198@qq.com>
2017-11-16 18:03:38 +0800

    完善

--- config/app.php
+++ config/app.php
@@ -169,7 +169,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -205,7 +204,6 @@ return [
         'Config'       => Illuminate\Support\Facades\Config::class,
         'Cookie'       => Illuminate\Support\Facades\Cookie::class,
         'Crypt'        => Illuminate\Support\Facades\Crypt::class,
-        'DataTables'   => Yajra\DataTables\Facades\DataTables::class,
         'DB'           => Illuminate\Support\Facades\DB::class,
         'Eloquent'     => Illuminate\Database\Eloquent\Model::class,
         'Event'        => Illuminate\Support\Facades\Event::class,

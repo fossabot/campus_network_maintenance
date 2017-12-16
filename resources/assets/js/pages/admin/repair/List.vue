7959b55b8f3225667399bf3e59960d7e41283bb7
StarUDream <840175198@qq.com>
2017-12-16 20:27:54 +0800

    格式化代码

--- resources/assets/js/pages/admin/repair/List.vue
+++ resources/assets/js/pages/admin/repair/List.vue
@@ -4,40 +4,24 @@
             <el-form ref="search" :model="search" label-width="100px">
                 <el-row>
                     <el-col :md="6">
-                        <el-form-item label="状态">
-                            <el-select v-model="search.status_id">
-                                <el-option v-for="item in status" :key="item.value" :label="item.label" :value="item.value"></el-option>
-                            </el-select>
-                        </el-form-item>
+                        <el-form-item label="状态"><el-select v-model="search.status_id"><el-option v-for="item in status" :key="item.value" :label="item.label" :value="item.value"></el-option></el-select></el-form-item>
                     </el-col>
                     <el-col :md="6" v-if="admin.role_id == 9">
-                        <el-form-item label="分类">
-                            <el-select v-model="search.type_id">
-                                <el-option v-for="item in type" :key="item.id" :label="item.name" :value="item.id"></el-option>
-                            </el-select>
-                        </el-form-item>
+                        <el-form-item label="分类"><el-select v-model="search.type_id"><el-option v-for="item in type" :key="item.id" :label="item.name" :value="item.id"></el-option></el-select></el-form-item>
                     </el-col>
                     <el-col :md="6">
-                        <el-form-item label="报障人学号">
-                            <el-input v-model="search.user_id"></el-input>
-                        </el-form-item>
+                        <el-form-item label="报障人学号"><el-input v-model="search.user_id"></el-input></el-form-item>
                     </el-col>
                     <el-col :md="6">
-                        <el-form-item label="报障人手机">
-                            <el-input v-model="search.user_mobile"></el-input>
-                        </el-form-item>
+                        <el-form-item label="报障人手机"><el-input v-model="search.user_mobile"></el-input></el-form-item>
                     </el-col>
                 </el-row>
                 <el-row type="flex" justify="center">
                     <el-col :md="7">
-                        <el-form-item label="自定义时间">
-                            <el-date-picker v-model="search.time" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd" unlink-panels></el-date-picker>
-                        </el-form-item>
+                        <el-form-item label="自定义时间"><el-date-picker v-model="search.time" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd" unlink-panels></el-date-picker></el-form-item>
                     </el-col>
                     <el-col :md="5">
-                        <el-form-item>
-                            <el-button type="primary" @click="getData">查询</el-button>
-                        </el-form-item>
+                        <el-form-item><el-button type="primary" @click="getData">查询</el-button></el-form-item>
                     </el-col>
                 </el-row>
             </el-form>
@@ -46,50 +30,24 @@
             <el-table-column type="expand">
                 <template slot-scope="scope">
                     <el-form class="repair-detail" label-position="left" inline>
-                        <el-form-item label="开始报障时间">
-                            <span>{{ scope.row.created_at }}</span>
-                        </el-form-item>
-                        <el-form-item label="开始维修时间">
-                            <span>{{ scope.row.accepted_at }}</span>
-                        </el-form-item>
-                        <el-form-item label="维修完成时间">
-                            <span>{{ scope.row.repaired_at }}</span>
-                        </el-form-item>
-                        <el-form-item label="评价完成时间">
-                            <span>{{ scope.row.completed_at }}</span>
-                        </el-form-item>
+                        <el-form-item label="开始报障时间"><span>{{ scope.row.created_at }}</span></el-form-item>
+                        <el-form-item label="开始维修时间"><span>{{ scope.row.accepted_at }}</span></el-form-item>
+                        <el-form-item label="维修完成时间"><span>{{ scope.row.repaired_at }}</span></el-form-item>
+                        <el-form-item label="评价完成时间"><span>{{ scope.row.completed_at }}</span></el-form-item>
                     </el-form>
                 </template>
             </el-table-column>
             <el-table-column prop="id" label="id" width="50"></el-table-column>
             <el-table-column prop="status" label="状态" width="80"></el-table-column>
             <el-table-column prop="type.name" label="分类"></el-table-column>
-            <el-table-column label="地区">
-                <template slot-scope="scope">
-                    {{ scope.row.location.first }} {{ scope.row.location.second }}
-                </template>
-            </el-table-column>
+            <el-table-column label="地区"><template slot-scope="scope">{{ scope.row.location.first }} {{ scope.row.location.second }}</template></el-table-column>
             <el-table-column prop="user_room" label="故障房间号" width="100"></el-table-column>
             <el-table-column prop="user_id" label="报障人学号" width="150"></el-table-column>
             <el-table-column prop="user_name" label="报障人姓名" width="110"></el-table-column>
             <el-table-column prop="user_mobile" label="报障人手机号码" width="150"></el-table-column>
-            <el-table-column label="操作" width="110">
-                <template slot-scope="scope">
-                    <el-button size="mini" @click="$router.push('/repair/detail/' + scope.row.id)">查看 / 修改</el-button>
-                </template>
-            </el-table-column>
+            <el-table-column label="操作" width="110"><template slot-scope="scope"><el-button size="mini" @click="$router.push('/repair/detail/' + scope.row.id)">查看 / 修改</el-button></template></el-table-column>
         </el-table>
-        <el-pagination
-            layout="sizes, prev, pager, next, jumper, ->, total"
-            :total="total"
-            :page-sizes="[20, 50, 100, 200]"
-            :page-size="search.per"
-            :current-page="search.page"
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            style="margin-top: 20px;"
-        >
-        </el-pagination>
+        <el-pagination layout="sizes, prev, pager, next, jumper, ->, total" :total="total" :page-sizes="[20, 50, 100, 200]" :page-size="search.per" :current-page="search.page" @size-change="handleSizeChange" @current-change="handleCurrentChange" style="margin-top: 20px;"></el-pagination>
     </div>
 </template>
 
